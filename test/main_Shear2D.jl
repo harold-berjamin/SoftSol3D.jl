153760a683ae933ce8368fa894e99d4c2a998e8c
harold-berjamin <92039688+harold-berjamin@users.noreply.github.com>
2024-06-26 13:54:49 +0100

    2D export

--- test/main_Shear2D.jl
+++ test/main_Shear2D.jl
@@ -25,7 +25,7 @@ function main()
     mate = Material(
         ρ = 1.0e3, # 1.0e3 (kg/m³)
         μ = 2.684e3, # 2.684e3 (Pa)
-        β = 4.4, # 4.4
+        β = 0., # 4.4
         g = [0.0434, 0.0466, 0.2213], # [0.0434, 0.0466, 0.2213]
         ω = 2*π * [10^1, 10^2, 10^3], # 2*π * [10^1, 10^2, 10^3]
         ϵ = 0.9 # 0.9 (AC parameter)
@@ -34,7 +34,7 @@ function main()
         xmin = [-0.3, -0.3, -0.3], # [-0.5, -0.1, -0.1]
         xmax = [ 0.3,  0.3,  0.3], # [ 0.5,  0.1,  0.1]
         tmax = 0.1, # 0.18
-        Nx = [300, 300, 4], # [200, 200, 4]
+        Nx = [200, 200, 4], # [300, 300, 4]
         Nt = 0, # 0
         Co = 0.95, # 0.95 (Courant number)
         flux = "MUSCL", # "LLF", "MUSCL"
@@ -70,6 +70,35 @@ function main()
     df = DataFrame([xplot uplot], :auto)
     CSV.write("output/Result.csv", df)
     print("Results saved in output/Result.csv\n")
+
+    # Write 2D text output
+    dat2d = zeros(mesh.Nx[2]+1, 3)
+    k2d = trunc(Int, mesh.Nx[3]/2.) + 1
+    for i=1:mesh.Nx[1]+1
+        n2d = 1
+        for j=1:mesh.Nx[2]+1
+            Xij = mesh.X[i,j,k2d]
+            dat2d[n2d,1] = Xij[1]
+            dat2d[n2d,2] = Xij[2]
+            dat2d[n2d,3] = mesh.Qp[i,j,k2d][fpara]
+            n2d += 1
+        end
+        df = DataFrame(dat2d, :auto)
+        if i==1
+            CSV.write("output/ResultZ.csv", df, writeheader = false)
+        else
+            CSV.write("output/ResultZ.csv", df, writeheader = true, append = true)
+        end
+    end
+    open("output/ResultZ.csv", "r") do f
+        s1 = read(f, String)
+        s2 = replace(s1, "x1,x2,x3" => "")
+        open("output/ResultZ-2D.csv", "w") do g
+            write(g, s2)
+        end
+    end
+    rm("output/ResultZ.csv")
+    print("Results saved in output/ResultZ-2D.csv\n")
 
     # Write VTK output
     if fpara > 0
